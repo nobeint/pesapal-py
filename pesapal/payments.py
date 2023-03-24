5d8e01e7aee93dc5f359bfe69c365dab1de8744d
twais <brian.otieno709@gmail.com>
2023-03-24 21:43:23 +0300

    initial commit

---
+++ pesapal/payments.py
@@ -0,0 +1,174 @@
+import json
+import requests
+
+
+class PesaPal(object):
+    def __init__(self, consumer_key, consumer_secret):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.base_production_url = "https://pay.pesapal.com/v3/api"
+        self.base_demo_url = "https://cybqa.pesapal.com/pesapalv3/api"
+        self.headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
+
+    def authorize(self):
+        auth_payload = {
+            "consumer_key": self.consumer_key,
+            "consumer_secret": self.consumer_secret,
+        }
+        auth_response = requests.post(
+            f"{self.base_production_url}/Auth/RequestToken",
+            json.dumps(auth_payload),
+            headers=self.headers,
+        )
+        if auth_response.status_code == 200:
+            auth_data = json.loads(auth_response.content)
+            if auth_data["status"] == "200":
+                return {
+                    "status": "success",
+                    "token": auth_data["token"],
+                    "expiry": auth_data["expiryDate"],
+                }
+            else:
+                return {
+                    "status": "failed",
+                    "error": auth_data["error"],
+                    "message": auth_data["error"]["message"],
+                }
+        return {
+            "status": "failed",
+            "error": f"invalid HTTP status {auth_response.status_code}",
+            "message": f"invalid server response",
+        }
+
+    def register_ipn(self, token, ipn_url):
+        register_ipn_payload = {"url": ipn_url, "ipn_notification_type": "POST"}
+        self.headers["Authorization"] = f"Bearer {token}"
+        register_ipn_response = requests.post(
+            f"{self.base_production_url}/URLSetup/RegisterIPN",
+            json.dumps(register_ipn_payload),
+            headers=self.headers,
+        )
+        if register_ipn_response.status_code == 200:
+            ipn_data = json.loads(register_ipn_response.content)
+            if ipn_data["status"] == "200":
+                return {
+                    "status": "success",
+                    "ipn_url": ipn_data["url"],
+                    "ipn_id": ipn_data["ipn_id"],
+                }
+            return {
+                "status": "failed",
+                "error": ipn_data["error"],
+                "message": ipn_data["error"]["message"],
+            }
+
+        return {
+            "status": "failed",
+            "error": f"invalid HTTP status {register_ipn_response.status_code}",
+            "message": f"invalid server response",
+        }
+
+    def transact(
+        self,
+        token: str,
+        description: str,
+        transaction_id: str,
+        amount: int,
+        callback_url: str,
+        notification_id: str,
+        email_address: str,
+        phone_number: str,
+        country_code: str,
+        first_name: str,
+        last_name: str,
+        currency: str = "KES",
+    ):
+        transaction_payload = {
+            "id": transaction_id,
+            "currency": currency,
+            "amount": amount,
+            "description": description,
+            "callback_url": callback_url,
+            "notification_id": notification_id,
+            "billing_address": {
+                "email_address": email_address,
+                "phone_number": phone_number,
+                "country_code": country_code,
+                "first_name": first_name,
+                "last_name": last_name,
+            },
+        }
+        self.headers["Authorization"] = f"Bearer {token}"
+        transact_response = requests.post(
+            f"{self.base_production_url}/Transactions/SubmitOrderRequest",
+            json.dumps(transaction_payload),
+            headers=self.headers,
+        )
+        if transact_response.status_code == 200:
+            transaction_data = json.loads(transact_response.content)
+            if transaction_data["status"] == "200":
+                return {
+                    "status": "success",
+                    "order_tracking_id": transaction_data["order_tracking_id"],
+                    "merchant_reference": transaction_data["merchant_reference"],
+                    "redirect_url": transaction_data["redirect_url"],
+                }
+
+            return {
+                "status": "failed",
+                "error": transaction_data["error"],
+                "message": transaction_data["error"]["message"],
+            }
+
+        return {
+            "status": "failed",
+            "error": f"invalid HTTP status {transact_response.status_code}",
+            "message": f"invalid server response",
+        }
+
+    def get_transaction_status(self, token, order_tracking_id):
+        self.headers["Authorization"] = f"Bearer {token}"
+        get_transaction_status_response = requests.get(
+            f"{self.base_production_url}/Transactions/GetTransactionStatus?orderTrackingId={order_tracking_id}",
+            headers=self.headers,
+        )
+
+        if get_transaction_status_response.status_code == 200:
+            transaction_status_data = json.loads(
+                get_transaction_status_response.content
+            )
+            if transaction_status_data["status"] == "200":
+                if transaction_status_data["payment_status_description"] == "Completed":
+                    return {
+                        "status": "success",
+                        "error": {},
+                        "message": "transaction successful",
+                    }
+                elif transaction_status_data["payment_status_description"] == "Pending":
+                    return {
+                        "status": "pending",
+                        "error": {},
+                        "message": "transaction pending",
+                    }
+                else:
+                    return {
+                        "status": "failed",
+                        "error": transaction_status_data["message"],
+                        "message": "transaction failed",
+                    }
+
+            return {
+                "status": "failed",
+                "error": transaction_status_data["error"],
+                "message": transaction_status_data["error"]["message"],
+            }
+        transaction_status_data = json.loads(get_transaction_status_response.content)
+        error_message = json.loads(transaction_status_data["message"])
+        return {
+            "status": "unknown",
+            "error": f"invalid HTTP status {get_transaction_status_response.status_code}",
+            "message": error_message["error"]["message"],
+        }
---
+++ tests/test_payments.py
@@ -0,0 +1,210 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+import mock
+import json
+
+from pesapal.payments import PesaPal
+
+
+class TestPesapal(unittest.TestCase):
+    def setUp(self):
+        self.pesapal = PesaPal("test_consumer_key", "test_consumer_secret")
+
+    def test_authorization_successful(self):
+        with mock.patch("requests.post") as mocked_post:
+            mocked_post.return_value.status_code = 200
+            mocked_post.return_value.content = json.dumps(
+                {
+                    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYa",
+                    "expiryDate": "2021-08-26T12:29:30.5177702Z",
+                    "error": "",
+                    "status": "200",
+                    "message": "Request processed successfully",
+                }
+            )
+            actual_auth_request = self.pesapal.authorize()
+            self.assertEquals(actual_auth_request["status"], "success")
+            self.assertIsNotNone(actual_auth_request["token"])
+
+    def test_authorization_not_successful(self):
+        with mock.patch("requests.post") as mocked_post:
+            mocked_post.return_value.status_code = 200
+            mocked_post.return_value.content = json.dumps(
+                {
+                    "error": {"message": "some error message"},
+                    "status": "failed",
+                    "message": "Request not processed successfully",
+                }
+            )
+            actual_auth_request = self.pesapal.authorize()
+            self.assertEquals(actual_auth_request["status"], "failed")
+            self.assertIsNotNone(actual_auth_request["error"])
+
+    def test_register_ipn_successful(self):
+        with mock.patch("requests.post") as mocked_post:
+            mocked_post.return_value.status_code = 200
+            mocked_post.return_value.content = json.dumps(
+                {
+                    "url": "https://www.myapplication.com/ipn",
+                    "created_date": "2022-03-03T17:29:03.7208266Z",
+                    "ipn_id": "e32182ca-0983-4fa0-91bc-c3bb813ba750",
+                    "status": "200",
+                }
+            )
+            actual_register_ipn_request = self.pesapal.register_ipn(
+                "test_token", "https://test_url"
+            )
+            self.assertEquals(actual_register_ipn_request["status"], "success")
+            self.assertIsNotNone(actual_register_ipn_request["ipn_url"])
+
+    def test_register_ipn_not_successful(self):
+        with mock.patch("requests.post") as mocked_post:
+            mocked_post.return_value.status_code = 200
+            mocked_post.return_value.content = json.dumps(
+                {
+                    "error": {"message": "some error message"},
+                    "status": "failed",
+                    "message": "Request not processed successfully",
+                }
+            )
+            actual_register_ipn_request = self.pesapal.register_ipn(
+                "test_token", "https://test_url"
+            )
+            self.assertEquals(actual_register_ipn_request["status"], "failed")
+            self.assertIsNotNone(actual_register_ipn_request["error"])
+
+    def test_transact_successful(self):
+        with mock.patch("requests.post") as mocked_post:
+            mocked_post.return_value.status_code = 200
+            mocked_post.return_value.content = json.dumps(
+                {
+                    "order_tracking_id": "b945e4af-80a5-4ec1-8706-e03f8332fb04",
+                    "merchant_reference": "TEST1515111119",
+                    "redirect_url": "https://cybqa.pesapal.com/pesapaliframe/PesapalIframe3/Index/?OrderTrackingId=b945e4af-80a5-4ec1-8706-e03f8332fb04",
+                    "status": "200",
+                }
+            )
+            actual_transact_request = self.pesapal.transact(
+                "test_token",
+                "test payment",
+                "GSKKSG",
+                1000,
+                "https://test_callback_url",
+                "test_ipn_id",
+                "test@email.com",
+                "test_phone_number",
+                "KE",
+                "first_name",
+                "last_name",
+            )
+            self.assertEquals(actual_transact_request["status"], "success")
+            self.assertIsNotNone(actual_transact_request["order_tracking_id"])
+            self.assertIsNotNone(actual_transact_request["redirect_url"])
+
+    def test_get_transaction_status_successful(self):
+        with mock.patch("requests.get") as mocked_get:
+            mocked_get.return_value.status_code = 200
+            mocked_get.return_value.content = json.dumps(
+                {
+                    "payment_method": "Visa",
+                    "amount": 100,
+                    "created_date": "2022-04-30T07:41:09.763",
+                    "confirmation_code": "6513008693186320103009",
+                    "payment_status_description": "Success",
+                    "description": "Test description",
+                    "message": "Request processed successfully",
+                    "payment_account": "476173**0010",
+                    "call_back_url": "https://test.com/?OrderTrackingId=7e6b62d9-883e-440f-a63e-e1105bbfadc3&OrderMerchantReference=1515111111",
+                    "status_code": 2,
+                    "merchant_reference": "1515111111",
+                    "payment_status_code": "",
+                    "currency": "KES",
+                    "status": "200",
+                    "payment_status_description": "Completed"
+                }
+            )
+            actual_get_transaction_status_request = self.pesapal.get_transaction_status(
+                "test_token", "test_order_tracking_id"
+            )
+            self.assertEquals(
+                actual_get_transaction_status_request[
+                    "status"
+                ],
+                "success",
+            )
+            self.assertIsNotNone(
+                actual_get_transaction_status_request["message"]
+            )
+
+    def test_get_transaction_status_pending(self):
+        with mock.patch("requests.get") as mocked_get:
+            mocked_get.return_value.status_code = 200
+            mocked_get.return_value.content = json.dumps(
+                {
+                    "payment_method": "Visa",
+                    "amount": 100,
+                    "created_date": "2022-04-30T07:41:09.763",
+                    "confirmation_code": "6513008693186320103009",
+                    "payment_status_description": "Success",
+                    "description": "Test description",
+                    "message": "Request pending",
+                    "payment_account": "476173**0010",
+                    "call_back_url": "https://test.com/?OrderTrackingId=7e6b62d9-883e-440f-a63e-e1105bbfadc3&OrderMerchantReference=1515111111",
+                    "status_code": 2,
+                    "merchant_reference": "1515111111",
+                    "payment_status_code": "",
+                    "currency": "KES",
+                    "status": "200",
+                    "payment_status_description": "Pending"
+                }
+            )
+            actual_get_transaction_status_request = self.pesapal.get_transaction_status(
+                "test_token", "test_order_tracking_id"
+            )
+            self.assertEquals(
+                actual_get_transaction_status_request[
+                    "status"
+                ],
+                "pending",
+            )
+            self.assertIsNotNone(
+                actual_get_transaction_status_request["message"]
+            )
+
+    def test_get_transaction_status_failed(self):
+        with mock.patch("requests.get") as mocked_get:
+            mocked_get.return_value.status_code = 200
+            mocked_get.return_value.content = json.dumps(
+                {
+                    "payment_method": "Visa",
+                    "amount": 100,
+                    "created_date": "2022-04-30T07:41:09.763",
+                    "confirmation_code": "6513008693186320103009",
+                    "payment_status_description": "Success",
+                    "description": "Test description",
+                    "message": "Request failed",
+                    "payment_account": "476173**0010",
+                    "call_back_url": "https://test.com/?OrderTrackingId=7e6b62d9-883e-440f-a63e-e1105bbfadc3&OrderMerchantReference=1515111111",
+                    "status_code": 2,
+                    "merchant_reference": "1515111111",
+                    "payment_status_code": "",
+                    "currency": "KES",
+                    "status": "200",
+                    "payment_status_description": "Failed"
+                }
+            )
+            actual_get_transaction_status_request = self.pesapal.get_transaction_status(
+                "test_token", "test_order_tracking_id"
+            )
+            self.assertEquals(
+                actual_get_transaction_status_request[
+                    "status"
+                ],
+                "failed",
+            )
+            self.assertIsNotNone(
+                actual_get_transaction_status_request["message"]
+            )
