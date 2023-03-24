f23ce584dca8a5c8e3f8bd8a261d3596fbb7fc43
twais <brian.otieno709@gmail.com>
2023-03-25 00:29:44 +0300

    update author email

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     description="A minimalist python library that integrates with PesaPal's API 3.0 - JSON APIs (https://developer.pesapal.com/how-to-integrate/e-commerce/api-30-json/api-reference)",
     author="Brian Owino Otieno",
-    author_email="brian@sabce.co.ke",
+    author_email="brian.otieno709@gmail.com",
     packages=["pesapal_py"],
     install_requires=["requests"],
 )
