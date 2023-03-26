ee5667dfb2e8368b4d7dadda00c994697e7be5ca
twais <brian.otieno709@gmail.com>
2023-03-27 01:53:12 +0300

    update how pypi description is accessed

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="pesapal_py",
     version="0.0.1",
     description="A minimalist python library that integrates with PesaPal's API 3.0 - JSON APIs (https://developer.pesapal.com/how-to-integrate/e-commerce/api-30-json/api-reference)",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Brian Owino Otieno",
     author_email="brian.otieno709@gmail.com",
     packages=["pesapal_py"],
